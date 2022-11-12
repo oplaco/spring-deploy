50b86038640c6f6cd04c93aa2c16dec9d84111d5
oplaco <gabriel.alfonsin.espin@gmail.com>
2022-11-12 17:58:02 +0100

    spring heroku app config

---
+++ src/main/java/com/example/springdeploy/HelloController.java
@@ -0,0 +1,38 @@
+package com.example.springdeploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")//Nos permite inyectar una propiedad del archivo application.properties
+    String message;
+    //Hay que asignar una URI al método para que sea accesible desde internet
+    @GetMapping("/hola")
+    public String holaMundo(){
+        System.out.println(message);
+        return "Hola mundo que tal vamos";
+
+    }
+
+    @GetMapping("/bootstrap")
+    public String bootstrap(){
+        return """
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Bootstrap demo</title>
+                    <p>Un párrafo random</p>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Hello, world!</h1>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+                  </body>
+                </html>""";
+    }
+}
